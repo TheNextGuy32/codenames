09fa0265c3a2019a5b4f098297f235a0a7a24b06
Oliver <oliverbarnum32@gmail.com>
2017-11-19 13:50:43 -0500

    Make the game

--- train.py
+++ train.py
@@ -1,4 +1,6 @@
 from gensim.models import Word2Vec
+import os
+import re
 import random
 import keyCardReader
 
@@ -13,6 +15,9 @@ for c in range(len(codenames)):
 
 	codenameSentences.append([codename])
 	codenames[c] = codename
+
+keyCards = keyCardReader.readFromFile()
+keyCard = keyCards[int(random.random()*(len(keyCards)))]	
 
 # trainedModelFile = "MODEL_FILE.txt"
 # model = Word2Vec.load(trainedModelFile)
@@ -60,67 +65,66 @@ def generateHint(board, keyCard):
 def isGameComplete(board):
 	return 0
 
-def guessCoordinate(x,y,board,keyCard):
-	z = (y*5)+x
+def guessCoordinate(z,board,keyCard):
+	if board[z] == "NEU" or board[z] == "RED" or board[z]=="BLU" or board[z] =="DIE":
+		print("You've already guessed %i,%i." % (x,y))
+		return 0
+
 	agentType = keyCard[z]
 	if agentType == 0:
 		#  Neutral
-		print("%s was NEUTRAL! Too bad!" % board[z])
+		print("%s was NEUTRAL! Too bad!\n" % board[z])
 		board[z] = "NEU"
 	elif agentType == 1:
 		#  Blue
-		print("%s was a BLU agent! Good job!" % board[z])
+		print("%s was a BLU agent! Good job!\n" % board[z])
 		board[z] = "BLU"
 	elif agentType == 2:
 		#  Red
-		print("%s was a RED agent! Too bad!" % board[z])
+		print("%s was a RED agent! Too bad!\n" % board[z])
 		board[z] = "RED"
 	else:
 		#  Assassin
-		print("%s was an assassin! You lose!" % board[z])
+		print("%s was an ASSASSIN! You lose!\n" % board[z])
 		board[z] = "DIE"
 		return 1
 		
 	return isGameComplete(board)
 
+def findCodenameCoord(response):
+	for c in range(len(board)):
+		if response == board[c]:
+			return c
+	return -1
+
 def game(board,keyCard):
 	#  Welcome Message
 	print("Welcome to Codenames!\n")
 	print("Use the computer generated hints to try to find your agents!")
-	print("Input the coordinate of the codename you think the bot is referring to.")
-	print("Input is X: 0-4 and Y: 0-4 inclusive.\n")
-	print("For instance, %s is at 0,1" % board[5])
+	print("Input the codename you think the bot is referring to.\n")
 
 	#  Main game loop
 	gameComplete = 0
 	while not gameComplete:
 		printBoard(board,longestWordLength)
-		print("Hint: %s" % generateHint(board,keyCard))	
+		print("\nHint: %s" % generateHint(board,keyCard))	
 		
 		#  Keep looping until they give good input
-		x = -1
-		y = -1
-		while x == -1 or y == -1:
-			response = input()
-			
-			#  Wrong input length
-			if len(response) != 3:
-				print("Response must be in 1,2 format.")
-				pass
-			
-			coords = response.split(',')
-			attX = int(coords[0])
-			attY = int(coords[1])
+		response = ""
+		coord = -1
+		while response == "" and coord == -1:
+			attemptedResponse = input().lower()
+			if re.match("^[a-z]", attemptedResponse):
+				coord = findCodenameCoord(attemptedResponse)
+				if coord != -1:
+					response = attemptedResponse
+				else:
+					print("%s not found on board."%attemptedResponse)
+			else:
+				print("Incorrect input format.")
 
-			#  Wrong input range
-			if attX > 4 or attX < 0 or attY > 4 or attX < 0:
-				print("Response coords must be 0-4 inclusive.")
-				pass
-			x = attX
-			y = attY
-			
 		#  Attempt game and see if we are done
-		gameComplete = guessCoordinate(x,y,board,keyCard)
-
-keyCard = {}
+		os.system('cls' if os.name == 'nt' else 'clear')
+		gameComplete = guessCoordinate(coord,board,keyCard)
+	print("Game Over! Thanks for playing!")
 game(board,keyCard)
