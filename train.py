78bf20099bebefd46c8cb0546ddce5ae84bc12ee
Oliver <oliverbarnum32@gmail.com>
2017-11-19 12:58:31 -0500

    Add title

--- train.py
+++ train.py
@@ -20,16 +20,35 @@ for c in range(len(codenames)):
 
 
 def createBoard():
+	longestWordLength = 1
 	board = [""] * 25 
 	freeCodename = [1]  * len(codenames)
 	for i in range(25):
 		while 1:
-			codenameIndex = int(random.random()*(len(codenames)+1))	
+			codenameIndex = int(random.random()*(len(codenames)))	
 			if freeCodename[codenameIndex]:
 				board[i] = codenames[codenameIndex]
+				longestWordLength = max(longestWordLength,len(board[i]))
 				freeCodename[codenameIndex] = 0
 				break
-	return board
+	return (board,longestWordLength)
 
-board = createBoard()
+def printBoard(board, longestWordLength):
+	#  Title spacing
+	spacing = 3
+	boardLength = (longestWordLength * 5) + (spacing*3)
+	boardTitle = "====  Codenames  ====="
+	titleSpaces = int((boardLength/2) - (len(boardTitle)/2))
+	print("%s%s"%(titleSpaces*' ',boardTitle))
 
+	#  Print codenames
+	for c in range(len(board)):
+		if int(c % 5) == 0 and c != 0:
+			print("")
+		numberSpaces = longestWordLength - len(board[c])
+		spaces = (numberSpaces+spacing) * ' '
+		print("%s%s" % (board[c],spaces), end='')
+	print("")
+
+board, longestWordLength = createBoard()
+printBoard(board,longestWordLength)
