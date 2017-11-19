57c949041cfc3b131e4b7c9f91eeb1a519531967
Gregory Goh (Liang) <gohliang@gmail.com>
2017-11-19 13:22:11 -0500

    Rename maps to key cards

---
+++ keyCardReader.py
@@ -0,0 +1,36 @@
+"""
+mapper.py
+
+This file handles the reading of key card files.
+"""
+
+from collections import Counter
+
+def verify(keyCard):
+    counts = Counter(keyCard)
+    return counts[0] == 7 and counts[1] == 9 and counts[2] == 8 and counts[3] == 1
+
+def main():
+    keyCards = []
+    with open('codenameKeyCard20.txt', 'r') as keyCardFile:
+        # First line contains number of key cards in the file
+        count = int(keyCardFile.readline())
+        while count > 0:
+            line = keyCardFile.readline().rstrip('\n')
+            # Every key card is separated by ===
+            if line == '===':
+                keyCard = []
+                for i in range(5):
+                    rowStr = keyCardFile.readline().rstrip('\n').split()
+                    rowInt = [int(rowStr[j]) for j in range(len(rowStr))]
+                    keyCard = keyCard + rowInt
+                if verify(keyCard):
+                    keyCards.append(keyCard)
+                count -= 1
+
+    print(len(keyCards))
+    for kc in keyCards:
+        print(kc)
+
+if __name__ == '__main__':
+    main()
