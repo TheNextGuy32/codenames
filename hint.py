895d6663ef7bbc15f60106b0efbdb42a4931f685
James-Reilly <james.reilly7777@gmail.com>
2017-11-21 20:15:38 -0500

    Testing word2vec/wordnet

--- hint.py
+++ hint.py
@@ -1,5 +1,10 @@
 from gensim.models import Word2Vec
-def generateHint(board, keyCard):
-    hint = "ballroom"
-    numberReferences = 1
-    return (hint, numberReferences)
+
+ignored_words = ["BLU", "RED", "NEU", "DIE"]
+
+def generateHint(player, board, keyCard, model):
+    zipped = zip(board, keyCard)
+    positive = list(map(lambda y: y[0], filter(lambda x: x[0] not in ignored_words and x[1] == player, zipped)))
+    negative = list(map(lambda y: y[0], filter(lambda x: x[0] not in ignored_words and x[1] != player, zipped)))
+    
+    return model.generateHint(positive, negative)
