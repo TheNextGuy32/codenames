59781268da8b62bfaabc58090df77615495f29f8
Gregory Goh (Liang) <gohliang@gmail.com>
2017-11-22 23:55:09 -0500

    Generate hint using spaCy from first word of list

--- spacyPredictor.py
+++ spacyPredictor.py
@@ -8,26 +8,43 @@ import spacy
 import numpy
 vectorModel = spacy.load('en_vectors_web_lg')
 
+def vectorize(words):
+    # Generate vectors for each word based on spaCy's model
+    vectors = [vectorModel.vocab[word].vector for word in words]
+    return vectors
+
+def overlap(hint, words):
+    overlap = False
+    for codename in words:
+        overlap = overlap or (hint in codename or codename in hint)
+    return overlap
+
 def similarity(wv1, wv2):
     # compare similarity between two numpy vectors
     if (numpy.linalg.norm(wv1) == 0) or (numpy.linalg.norm(wv2) == 0):
         return 0.0
     return numpy.dot(wv1, wv2) / numpy.linalg.norm(wv1) * numpy.linalg.norm(wv2)
 
-def mostSimilar(vector, words):
+def tenMostSimilar(vector):
     # Return the most similar word compared to the given vector
     similarWords = []
     for w in vectorModel.vocab:
         if (w.has_vector) and (w.orth_.islower()) and (w.lower_ not in words):
             similarWords.append(w)
     similarWords.sort(key=lambda w: similarity(w.vector, vector), reverse=True)
-    return similarWords[0].orth_
+    return similarWords[:10]
 
-def vectorize(words):
-    # Generate vectors for each word based on spaCy's model
-    vectors = [vectorModel.vocab[word].vector for word in words]
-    return vectors
+def generateHint(words):
+    if len(words) > 0:
+        vectors = vectorize(words)
+        # Only generates hint for the first word in the list
+        similar = tenMostSimilar(vectors[0])
+        clues = [c.orth_ for c in similar]
+        for clue in clues:
+            if not overlap(clue, words):
+                return clue
 
 if __name__ == '__main__':
-    vectors = vectorize(['clown', 'sphere', 'money'])
-    print(mostSimilar(vectors[2], ['clown', 'sphere', 'money']))
+    words = ['clown', 'sphere', 'money']
+    hint = generateHint(words)
+    print(hint)
