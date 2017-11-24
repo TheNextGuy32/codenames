e703d4f4d37439ad1e3a670974915a90b175687a
Gregory Goh (Liang) <gohliang@gmail.com>
2017-11-24 18:06:23 -0500

    Finalize spaCy hint generator

--- spacyHint.py
+++ spacyHint.py
@@ -7,7 +7,37 @@ the words using spaCy word vector models.
 
 import spacy
 import numpy
-vectorModel = spacy.load('en_vectors_web_lg')
+
+def similarity(wv1, wv2):
+    """ compare similarity between two numpy vectors
+    Adapted from wordembeddings lab from class
+    """
+    if (numpy.linalg.norm(wv1) == 0) or (numpy.linalg.norm(wv2) == 0):
+        return 0.0
+    return numpy.dot(wv1, wv2) / numpy.linalg.norm(wv1) * numpy.linalg.norm(wv2)
+
+def tenMostSimilar(vector, vectorModel):
+    """ Return the 10 most similar words compared to the given vector
+    Adapted from wordembeddings lab from class
+    """
+    similarWords = []
+    for w in vectorModel.vocab:
+        if (w.has_vector) and (w.orth_.islower()) and (w.lower_ not in words):
+            similarWords.append(w)
+    similarWords.sort(key=lambda w: similarity(w.vector, vector), reverse=True)
+    return similarWords[:10]
+
+def chooseHint(candidates, words):
+    """ Pick the first candidate hint that does not overlap the
+    current board, represented by words.
+
+    :param candidates:  List of candidate hints, as spaCy lexemes
+    :param words:       List of word strings
+    """
+    if len(candidates) > 0:
+        for clue in candidates:
+            if not overlap(clue.orth_, words):
+                return clue
 
 def overlap(hint, words):
     """ Check if the hint has overlaps or is overlapped
@@ -21,31 +51,10 @@ def overlap(hint, words):
         overlap = overlap or (hint in codename or codename in hint)
     return overlap
 
-def similarity(wv1, wv2):
-    # compare similarity between two numpy vectors
-    if (numpy.linalg.norm(wv1) == 0) or (numpy.linalg.norm(wv2) == 0):
-        return 0.0
-    return numpy.dot(wv1, wv2) / numpy.linalg.norm(wv1) * numpy.linalg.norm(wv2)
-
-def tenMostSimilar(vector):
-    # Return the 10 most similar word compared to the given vector
-    similarWords = []
-    for w in vectorModel.vocab:
-        if (w.has_vector) and (w.orth_.islower()) and (w.lower_ not in words):
-            similarWords.append(w)
-    similarWords.sort(key=lambda w: similarity(w.vector, vector), reverse=True)
-    return similarWords[:10]
-
-def chooseHint(candidates, words):
-    if len(candidates) > 0:
-        for clue in candidates:
-            if not overlap(clue.orth_, words):
-                return clue
-
-def filterBoard(board, keyCard):
+def filterBoard(board, keyCard, vectorModel):
     """ Organize the given codenames on the board,
     categorizing each word according to role,
-    then generating a spaCy representation of each word.
+    then generating a spaCy lexeme of each word.
     """
     blues = []
     reds = []
@@ -61,19 +70,64 @@ def filterBoard(board, keyCard):
     reds = [vectorModel.vocab[r] for r in reds if r != 'RED']
     return blues, reds, vectorModel.vocab[assassin]
 
+def selectReferences(blues, reds, threshold):
+    """ Select the best set of words to create a hint for,
+    based on the words' similarity with each other.
+
+    :param blues:       list of operatives on the blue team, as spaCy lexemes
+    :param reds:        list of operatives on the red team, as spaCy lexemes
+    :param threshold    similarity threshold between two words, from 0-1
+    """
+    plus = {}
+    minus = {}
+    score = {}
+    for i in range(len(blues)):
+        bi = blues[i]
+        plus[bi] = []
+        minus[bi] = []
+        for j in range(len(blues)):
+            if i != j:
+                bj = blues[j]
+                if bi.similarity(bj) > threshold:
+                    plus[bi].append(bj)
+        for k in range(len(reds)):
+            if i != k:
+                rk = reds[k]
+                if bi.similarity(rk) > threshold:
+                    minus[bi].append(rk)
+        score[bi] = len(plus[bi]) - len(minus[bi])
+    scoreVals = list(score.values())
+    bestScoreIndex = scoreVals.index(max(scoreVals))
+    bestKey = list(score.keys())[bestScoreIndex]
+    return [bestKey] + plus[bestKey]
+
+def averageVector(words):
+    """ Find the average of the vectors of each given word.
+
+    :param words: a list of spaCy lexemes
+    """
+    sum = None
+    for w in words:
+        if sum is None:
+            sum = w.vector
+        else:
+            sum += w.vector
+    return sum / len(words)
+
 def generateHint(board, keyCard):
-    blues, reds, assassin = filterBoard(board, keyCard)
-    for b in blues:
-        bv = b.vector
-        bw = b.orth_
-        similar = tenMostSimilar(bv)
-        hint = chooseHint(similar, board)
-        print(hint.orth_ + '\t' + str(hint.similarity(b)))
-        print(similarity(bv, hint.vector))
-        print(b.similarity(b))
+    """ Generate a hint based on spaCy's word vector model.
+    """
+    vectorModel = spacy.load('en_vectors_web_lg')
+    blues, reds, assassin = filterBoard(board, keyCard, vectorModel)
+    refs = selectReferences(blues, reds, 0.4)
+    v = averageVector(refs)
+    v -= assassin.vector
+    similar = tenMostSimilar(v, vectorModel)
+    hint = chooseHint(similar, board)
+    return (hint.orth_, len(refs))
 
 if __name__ == '__main__':
-    words = ['clown', 'sphere', 'money', 'box']
-    keyCard = [1, 1, 1, 1]
+    words = ['clown', 'sphere', 'money', 'cube', 'box']
+    keyCard = [1, 1, 1, 2, 3]
     hint = generateHint(words, keyCard)
     print(hint)
