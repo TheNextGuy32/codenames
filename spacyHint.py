eeeb695834e4f97cbedbae0c3ad5e0b6e653ada2
Gregory Goh (Liang) <gohliang@gmail.com>
2017-11-24 14:55:23 -0500

    Clean up hint checking

--- spacyHint.py
+++ spacyHint.py
@@ -1,19 +1,21 @@
 """
 spacyPredictor.py
 
-This file allows comparisons to the spaCy word vector models.
+This file allows generating hints by measuring
+the words using spaCy word vector models.
 """
 
 import spacy
 import numpy
 vectorModel = spacy.load('en_vectors_web_lg')
 
-def vectorize(words):
-    # Generate vectors for each word based on spaCy's model
-    vectors = [vectorModel.vocab[word].vector for word in words]
-    return vectors
-
 def overlap(hint, words):
+    """ Check if the hint has overlaps or is overlapped
+    by any word in words. (e.g. 'clown' in 'clowns')
+
+    :param hint:    The proposed hint string
+    :param words:   List of word strings
+    """
     overlap = False
     for codename in words:
         overlap = overlap or (hint in codename or codename in hint)
@@ -26,7 +28,7 @@ def similarity(wv1, wv2):
     return numpy.dot(wv1, wv2) / numpy.linalg.norm(wv1) * numpy.linalg.norm(wv2)
 
 def tenMostSimilar(vector):
-    # Return the most similar word compared to the given vector
+    # Return the 10 most similar word compared to the given vector
     similarWords = []
     for w in vectorModel.vocab:
         if (w.has_vector) and (w.orth_.islower()) and (w.lower_ not in words):
@@ -34,14 +36,17 @@ def tenMostSimilar(vector):
     similarWords.sort(key=lambda w: similarity(w.vector, vector), reverse=True)
     return similarWords[:10]
 
-def chooseHint(candidates):
+def chooseHint(candidates, words):
     if len(candidates) > 0:
-        clues = [c.orth_ for c in candidates]
-        for clue in clues:
-            if not overlap(clue, words):
+        for clue in candidates:
+            if not overlap(clue.orth_, words):
                 return clue
 
 def filterBoard(board, keyCard):
+    """ Organize the given codenames on the board,
+    categorizing each word according to role,
+    then generating a spaCy representation of each word.
+    """
     blues = []
     reds = []
     assassin = ''
@@ -52,18 +57,23 @@ def filterBoard(board, keyCard):
             reds.append(board[i])
         elif keyCard[i] == 1:
             blues.append(board[i])
-    blues = [b for b in blues if b != 'BLU']
-    reds = [r for r in reds if r != 'RED']
-    return blues, reds, assassin
+    blues = [vectorModel.vocab[b] for b in blues if b != 'BLU']
+    reds = [vectorModel.vocab[r] for r in reds if r != 'RED']
+    return blues, reds, vectorModel.vocab[assassin]
 
 def generateHint(board, keyCard):
     blues, reds, assassin = filterBoard(board, keyCard)
-    blueVectors = vectorize(blues)
-    redVectors = vectorize(reds)
-    assassin = vectorize([assassin])[0]
+    for b in blues:
+        bv = b.vector
+        bw = b.orth_
+        similar = tenMostSimilar(bv)
+        hint = chooseHint(similar, board)
+        print(hint.orth_ + '\t' + str(hint.similarity(b)))
+        print(similarity(bv, hint.vector))
+        print(b.similarity(b))
 
 if __name__ == '__main__':
     words = ['clown', 'sphere', 'money', 'box']
-    keyCard = [0, 1, 2, 3]
+    keyCard = [1, 1, 1, 1]
     hint = generateHint(words, keyCard)
     print(hint)
